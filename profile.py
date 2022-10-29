fa81c4b7e154283a7f89be48399bd5d069a6ae04
Erika Hunhoff <54562339+hunhoffe@users.noreply.github.com>
2022-10-29 14:13:02 -0600

    WIP profile used for creating image

---
+++ profile.py
@@ -0,0 +1,28 @@
+
+"""
+WIP profile, using for now just to set up image.
+
+Instructions:
+Under development, do not use.
+"""
+
+# Import the Portal object.
+import geni.portal as portal
+# Import the ProtoGENI library.
+import geni.rspec.pg as pg
+# Emulab specific extensions.
+import geni.rspec.emulab as emulab
+
+IMAGE='urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-STD'
+
+# Create a portal context, needed to defined parameters
+pc = portal.Context()
+
+# Create a Request object to start building the RSpec.
+request = pc.makeRequestRSpec()
+
+node = request.RawPC(name)
+node.disk_image = IMAGE
+node.hardware_type = "m510"
+
+pc.printRequestRSpec()
